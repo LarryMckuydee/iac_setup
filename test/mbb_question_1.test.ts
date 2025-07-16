204dd1195e7bf7bac645255f606e76a1bc28468a
Larry Mckuydee <larry.mckuydee@gmail.com>
2025-07-16 13:53:16 +0800

    Initial commit

---
+++ test/mbb_question_1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MbbQuestion1 from '../lib/mbb_question_1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mbb_question_1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MbbQuestion1.MbbQuestion1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
